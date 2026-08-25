8be002ebc0653e4cfafc9aa178f130253145c0ea
Swaraj Jagtap <swarajjagtap22@gmail.com>
2026-08-25 23:59:33 +0530

    Add files via upload

---
+++ POP.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Value1 = 0 , Value2 = 0 ,Result = 0;
+
+    cout<<"Enter First Number : \n";
+    cin>>Value1;
+
+    cout<<"Enter Second Number : \n";
+    cin>>Value2;
+
+    Result = Value1 + Value2;
+
+    cout<<"Addition is :"<<Result<<"\n";
+
+    return 0;
+}
